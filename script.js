133649df9c0c5320f66c4b259c84f5bae09633d9
Caio Machado <caio.machado3@fatec.sp.gov.br>
2025-05-13 21:30:26 -0300

    ajustes no html,css e scritp e adição do readme

--- script.js
+++ script.js
@@ -8,9 +8,17 @@ function toggleMode() {
   //substituir a imagem
   if (html.classList.contains("light")) {
     // se tiver light mode, adicionar a imagem light
-    img.setAttribute("src", "./assets/avatar-light.png");
+    img.setAttribute("src", "./assets/fatec_img.jpg");
+    img.setAttribute(
+      "alt",
+      "Foto de perfil do Estudante Caio Machado - Light Mode"
+    );
   } else {
     // se tiver sem light mode, manter a imagem normal
-    img.setAttribute("src", "./assets/avatar.png");
+    img.setAttribute("src", "./assets/fatec_img.jpg");
+    img.setAttribute(
+      "alt",
+      "Foto de perfil do Estudante Caio Machado - Dark Mode"
+    );
   }
 }
